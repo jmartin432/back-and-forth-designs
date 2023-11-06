afc9ad863f8456c6731a4db86c21afd1f3a19f65
Justin <info@justinlmartin.com>
2023-11-05 22:19:56 -0800

    outline

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,26 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Back and Forth Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+      <h1>Back and Forth</h1>
+      <h2>Designs and Dry Goods</h2>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+        <RouterLink to="/meeting">Meeting</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
+
+  <footer>
+
+  </footer>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
+<!-- <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
-</script>
+</script> -->
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>This is a Home Page</h1>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+  }
+}
+</style>
