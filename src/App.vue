d327b74ffcedfd05ac8ae7fed50714f652058b72
Justin <info@justinlmartin.com>
2023-11-05 22:46:24 -0800

    basic outline

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Back and Forth Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Back and Forth Logo" class="logo" src="@/assets/images/logo.svg" width="125" height="125" />
 
     <div>
       <h1>Back and Forth</h1>
@@ -31,6 +30,16 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
+}
+
+h1 {
+  font-size: 48px;
+  color: purple;
+}
+
+h2 {
+  font-size: 36px;
+  color: green;
 }
 
 .logo {
